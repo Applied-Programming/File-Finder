1ceaf7b967cf215828988e35941c21908d6072b5
Applied-Programming <aniruddhastapas@gmail.com>
2017-01-27 23:55:48 +0530

    Update

--- src/filefinder/FileFinder.java
+++ src/filefinder/FileFinder.java
@@ -3,84 +3,77 @@ package filefinder;
 import java.io.*;
 import java.util.*;
 
-class FileFinder{
+class FileFinder {
 
-static String ConvertFilesToString(File St)
-{
-	String Obj=St.getPath();
-	return Obj;
-}
+    static String ConvertFilesToString(File St) {
+        String strObj = St.getPath();
+        return strObj;
+    }
 
-public static void main (String[]args){
-	int i;
-	int Flag=1;
-	File Obj=new File(".");
-	String FileName="user.dat";
-	File []Arry=Obj.listRoots();
+    public static void main(String[] args) {
+        int i;
+        int Flag = 1;
+        File strObj = new File(".");
+        String FileName = "user.dat";
+        File[] Arry = strObj.listRoots();
 
-	Stack stack=new Stack();
+        Stack stack1 = new Stack();
 
-	System.out.println("Please Wait...");
+        System.out.println("Please Wait...");
 
-	for (int a=0;a<Arry.length&&Flag==1;a++)
-	{
+        for (int a = 0; a < Arry.length && Flag == 1; a++) {
 
+            if (Arry[a].exists()) {
 
-		if (Arry[a].exists())
-		{
+                String Str1 = Arry[a].getPath();
+                File O = new File(Str1);
+                File[] Arr = O.listFiles();
 
+                for (i = 0; i < Arr.length; i++) {
+                    if (Arr[i].isDirectory() && Arr[i].canRead()) {
+                        stack1.push(Arr[i]);
+                    } else {
+                        if (Arr[i].isFile()) {
+                            String StringObj = Arr[i].getName();
+                            if (FileName.equalsIgnoreCase(StringObj) && Arr[i].canRead()) {
+                                Flag = 0;
+                                System.out.println("File Found");
+                                System.out.println(Arr[i].getPath());
+                            }
+                        }
 
-	String Str=Arry[a].getPath();
-	File O=new File(Str);
-	File [] Arr= O.listFiles();
+                    }
+                }
 
-	for (i=0;i<Arr.length;i++)
-	if (Arr[i].isDirectory()&&Arr[i].canRead())
-		stack.push(Arr[i]);
-	else
-	{
-		if (Arr[i].isFile()) {
-		String StringObj=Arr[i].getName();
-	if(FileName.equalsIgnoreCase(StringObj)&&Arr[i].canRead()){
-			   Flag=0;
-			   System.out.println("File Found");
-			   System.out.println(Arr[i].getPath());
-			   }//if
-		   }//outer if
+                while (!stack1.empty() && Flag == 1) {
+                    try {
+                        O = new File(ConvertFilesToString((File) stack1.pop()));
+                        Arr = O.listFiles();
 
-	}
+                        for (i = 0; i < Arr.length; i++) {
+                            if (Arr[i].isDirectory() && Arr[i].canRead()) {
+                                stack1.push(Arr[i]);
+                            }
+                            if (Arr[i].isFile() && Arr[i].canRead()) {
+                                String StringObj = Arr[i].getName();
+                                if (FileName.equalsIgnoreCase(StringObj)) {
+                                    Flag = 0;
+                                    System.out.println("File Found");
+                                    System.out.println(Arr[i].getPath());
+                                }
+                            }
 
+                        }
+                    }
+                    catch (Exception e) {
+                    }
 
-	while (!stack.empty()&&Flag==1)
-		{
-try{
-	O=new File(ConvertFilesToString((File)stack.pop()));
-	Arr=O.listFiles();
+                }
+            }
+        }
 
-for (i=0;i<Arr.length;i++)
-{
-if (Arr[i].isDirectory()&&Arr[i].canRead())
-	stack.push(Arr[i]);
-if (Arr[i].isFile()&&Arr[i].canRead()) {
-	String StringObj=Arr[i].getName();
-	if(FileName.equalsIgnoreCase(StringObj)){
-	   Flag=0;
-	   System.out.println("File Found");
-		   System.out.println(Arr[i].getPath());
-	   }//if
-   }//outer if
-
-			}//for Loop
-}//try
-catch (Exception e){
-}
-
-
-		}//while Loop
-	}//outer If
-	}//Outer For Loop
-
-if (Flag==1)
-System.out.println("File "+FileName+" Does not Exists");
-}
+        if (Flag == 1) {
+            System.out.println("File " + FileName + " Does not Exists");
+        }
+    }
 }
